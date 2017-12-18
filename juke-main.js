6779bc274eedb60c7367a6684bcb3a5283924ea8
Jahaziel.vaz <jahaziel.vaz@gmail.com>
2017-12-18 01:03:35 -0700

    I added a practice combined html file called htmlAudioPractice.html to practice event listeners and adding audio

--- juke-main.js
+++ juke-main.js
@@ -4,10 +4,35 @@
 
 // document.getElementById("play").addEventListener(click,)
 
+// (function(exports){
+//
+//
+//
+//
+//
+// })(this);
 
-//QUICK EVENT LISTENERS PRACTICE
+//MAIN CONSTRUCTOR
+function Jukebox(pause, ff, rewind, stop){
+  this.play = [];
+  this.pause = pause;
+  this.ff = ff;
+  this.rewind = rewind;
+  this.stop = stop;
+};
 
-document.getElementById("play").addEventListener("click", function(){
-  // document.getElementById("song-name").innerHTML = "THIS IS JUST A TEST";
-  alert("Hello World");
-});
+
+//PLAY BUTTON
+Jukebox.prototype.songFeeder = function(addSong){
+  var innerSong = this.play.push(addSong);
+  return innerSong;
+};
+
+Jukebox.prototype.songExecute = function(addSong){
+  return songFeeder(addSong);
+};
+
+
+var newSong = new Jukebox();
+
+newSong.songFeeder("Billy Jean");
