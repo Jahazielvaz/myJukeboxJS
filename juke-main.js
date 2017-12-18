abe1e0e2d5401e598af5ea6530b116c1c7a2f5ce
Jahaziel <jahaziel.vaz@gmail.com>
2017-12-17 19:12:56 -0700

    First Commit

---
+++ juke-main.js
@@ -0,0 +1,13 @@
+// var song = new song({
+//   urls: ["greenChildren.mp3"]
+// }).play();
+
+// document.getElementById("play").addEventListener(click,)
+
+
+//QUICK EVENT LISTENERS PRACTICE
+
+document.getElementById("play").addEventListener("click", function(){
+  // document.getElementById("song-name").innerHTML = "THIS IS JUST A TEST";
+  alert("Hello World");
+});
