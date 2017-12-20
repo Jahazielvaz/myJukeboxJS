aca4f5918896c1ffbaad6579a6184eec672ea885
Jahaziel Vazquez <jahaziel.vaz@gmail.com>
2017-12-19 22:39:34 -0700

    Add files via upload
    I uploaded 2 more songs, and hard coded november rain to the play and pause button.

--- juke-main.js
+++ juke-main.js
@@ -36,3 +36,33 @@ Jukebox.prototype.songExecute = function(addSong){
 var newSong = new Jukebox();
 
 newSong.songFeeder("Billy Jean");
+
+
+
+//SONG BANK
+
+var novemberRain = document.querySelector("#novemberRain");
+var greenChildren = document.querySelector("#greenChildren");
+var augustana = document.querySelector("augustana");
+
+var songList = [novemberRain, greenChildren, augustana];
+
+var playButton = document.getElementById("play");
+var pauseButton = document.getElementById("pause");
+var ffButton = document.getElementById("fast-forward");
+
+playButton.addEventListener("click", function(){
+  novemberRain.play();
+  document.getElementById("song-name").innerHTML = "Guns N Roses - November Rain Solo" + "<br>" + "Playing";
+});
+
+pauseButton.addEventListener("click", function(){
+  novemberRain.pause();
+    document.getElementById("song-name").innerHTML = "Guns N Roses - November Rain Solo" + "<br>" + "Paused";
+});
+
+ffButton.addEventListener("click", function(){
+  for(i = 0; i < songList.length; i++){
+    return songList[i];
+  }
+})
